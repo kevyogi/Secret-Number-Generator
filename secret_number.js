5743cdb46a5deabef2f7af46607c0d41259ade0d
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-09-13 14:25:11 -1000

    all tests passing

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
-  
+  var mySecretNumber;
+  return function (){
+    if(mySecretNumber === undefined){
+      mySecretNumber = Math.floor(Math.random()*1000000);
+    }
+    return mySecretNumber;
+  }
 };
+
