190e43eaa543d9c643203a35d14f7514407d2100
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-09-13 16:07:23 -1000

    fixed +1 issue

--- secret_number.js
+++ secret_number.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function() {
-  var mySecretNumber = Math.floor(Math.random()*1000000 + 1)
+  var mySecretNumber = Math.floor(Math.random()*1000001)
   return function (){
     return mySecretNumber;
   }
